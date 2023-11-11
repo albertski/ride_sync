c7e8fa8d499a8def68c2105b98899a166cf3e0f7
Albert Jankowski <albertski84@yahoo.com>
2023-11-11 13:24:39 -0600

    Create Ride spec

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Ride, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:start_address).class_name('Address') }
+    it { should belong_to(:destination_address).class_name('Address') }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:start_address) }
+    it { should validate_presence_of(:destination_address) }
+  end
 end
