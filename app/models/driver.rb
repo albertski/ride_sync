d6d9556f267ecfe35ab653d58eae67a371b9b472
Albert Jankowski <albertski84@yahoo.com>
2023-11-11 13:22:15 -0600

    Add Driver specs

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
+
 class Driver < ApplicationRecord
-  belongs_to :home_address, class_name: 'Address'
-  validates :first_name, :last_name, :home_address, presence: true
-  validates_uniqueness_of :first_name, scope: [:last_name, :home_address]
+  belongs_to :home_address, class_name: 'Address',  foreign_key: :home_address_id
+  validates :first_name, :last_name, :home_address, presence: true, uniqueness: true
 end
