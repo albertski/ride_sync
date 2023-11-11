1bc866ed897857b116c9af8faee7487de3902c12
Albert Jankowski <albertski84@yahoo.com>
2023-11-10 18:52:26 -0600

    Formatting factory

--- spec/factories/address.rb
+++ spec/factories/address.rb
@@ -2,11 +2,11 @@
 
 FactoryBot.define do
   factory :address do
-    address1 { Faker::Address::street_address }
-    address2 { Faker::Address::street_address }
-    city     { Faker::Address.city }
-    state    { Faker::Address.state }
-    zip_code  { Faker::Address.zip_code }
-    country  { Faker::Address.country }
+    address1   { Faker::Address::street_address }
+    address2   { Faker::Address::street_address }
+    city       { Faker::Address.city }
+    state      { Faker::Address.state }
+    zip_code   { Faker::Address.zip_code }
+    country    { Faker::Address.country }
   end
 end
